fbc83e566674d128c6adfaae1e386e4e9c29c203
Mensur Kılıç <93288555+Mkrealy@users.noreply.github.com>
2022-07-06 17:50:50 +0300

    js
    navbar button edit

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -2,13 +2,9 @@ const menuToggle = document.querySelector('.menu-toggle');
 const navigation = document.querySelector('.navigation');
 const aLink = document.querySelectorAll('.a-link');
 menuToggle.onclick = ()=>{
-    navigation.classList.toggle('open'),
-    aLink[0].setAttribute("id", "display-none"),
-    aLink[1].setAttribute("id", "display-none"),
-    aLink[2].setAttribute("id", "display-none"),
-    aLink[3].setAttribute("id", "display-none")
+    navigation.classList.toggle('open')
     
-    ;};
+    };
 const listItems = document.querySelector('list-item');
 listItems.forEach( item => {
     item.onclick = () =>{
@@ -17,11 +13,3 @@ listItems.forEach( item => {
             item.classList.add('active');
         }
 });
-// aLink.forEach(item =>{
-//    item.onclick= () =>{
-//     aLink.forEach(item =>
-//         item.classList.remove('display-none'));
-//         item.classList.add('display-none');
-   
-//    }
-// });
